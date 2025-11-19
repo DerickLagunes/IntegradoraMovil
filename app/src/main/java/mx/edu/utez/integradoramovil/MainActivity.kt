9e64fb6053c029bbb442f2d89107e9f858324f61
Derick Axel Lagunes Ramirez <20243ds093@utez.edu.mx>
2025-11-19 15:27:19 -0600

    Estoy suplantando una identidad

--- app/src/main/java/mx/edu/utez/integradoramovil/MainActivity.kt
+++ app/src/main/java/mx/edu/utez/integradoramovil/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             IntegradoraMovilTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Androidsdsd",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
